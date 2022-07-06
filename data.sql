aab99f4f7c96af1c9d2cdb94d063de2b960043a1
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-07-06 22:48:50 +0200

    Modify your inserted animals to include owner information (owner_id)

--- data.sql
+++ data.sql
@@ -37,3 +37,10 @@ Modify inserted animals so it includes the species_id value:
 */
 UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
 UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+/* Modify your inserted animals to include owner information (owner_id) */
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon','Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon','Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander','Squirtle','Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon','Boarmon');
