8f77745078e8d85c0121ec7c7b856f101d7eecd3
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-07-06 22:44:39 +0200

    insert data into owners table

--- data.sql
+++ data.sql
@@ -17,3 +17,12 @@ VALUES
     ('Boarmon', '2005-06-07', 7, true, 20.4),
     ('Blossom', '1998-10-13', 3, true, 17),
     ('Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO owners (full_name,age)
+ VALUES 
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
